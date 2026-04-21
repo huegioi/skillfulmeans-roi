02cf28c14ef1c6a8e20e3f219a1437a5cc7ab587
William Jackson <75849493+huegioi@users.noreply.github.com>
2026-04-21 17:15:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '25mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'roi-calculator.html'));
